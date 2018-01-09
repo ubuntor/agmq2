f29c3acb6a5051b0aee77f393a4688dd639d05b4
Samuel Kim <ubuntor2000@gmail.com>
2018-01-09 15:14:28 -0500

    larger font for if

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A puzzle game. (Not to be confused with the IDE.)
 
+Fonts by Eeve Somepx.
+
 Heavily inspired by Baba Is You and Sig.NULL, with a dash of Zachtronics.
--- game.lua
+++ game.lua
@@ -1,31 +1,25 @@
 local game = {}
 
 function game.keypressed(key, level)
-    -- blargh this is ugly
+    local id, quad, x, y, rot = unpack(level.player)
     if key == "up" then
-        local id, quad, x, y, _ = unpack(level.player)
         y = y-1
-        level.tileBatch:set(id, quad, x*32+16, y*32+16, 0, 1, 1, 16, 16)
-        level.player = {id, quad, x, y, 0}
+        rot = 0
     end
     if key == "down" then
-        local id, quad, x, y, _ = unpack(level.player)
         y = y+1
-        level.tileBatch:set(id, quad, x*32+16, y*32+16, math.rad(180), 1, 1, 16, 16)
-        level.player = {id, quad, x, y, 2}
+        rot = 2
     end
     if key == "left" then
-        local id, quad, x, y, _ = unpack(level.player)
         x = x-1
-        level.tileBatch:set(id, quad, x*32+16, y*32+16, math.rad(270), 1, 1, 16, 16)
-        level.player = {id, quad, x, y, 3}
+        rot = 3
     end
     if key == "right" then
-        local id, quad, x, y, _ = unpack(level.player)
         x = x+1
-        level.tileBatch:set(id, quad, x*32+16, y*32+16, math.rad(90), 1, 1, 16, 16)
-        level.player = {id, quad, x, y, 1}
+        rot = 1
     end
+    level.tileBatch:set(id, quad, x*32+16, y*32+16, math.rad(90*rot), 1, 1, 16, 16)
+    level.player = {id, quad, x, y, rot}
 end
 
 
